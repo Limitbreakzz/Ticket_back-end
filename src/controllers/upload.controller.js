0de8a4127f78a50f439772166efd4b5c3500a89e
Limitbreakzz <lomphongsrathongmee@gmail.com>
2026-08-28 11:04:26 +0700

    chore: update dependencies and upload controller

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -1,12 +1,4 @@
 const prisma = require('../prisma');
-const cloudinary = require('cloudinary').v2;
-const fs = require('fs');
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
 
 exports.uploadFile = async (req, res) => {
   try {
@@ -17,20 +9,8 @@ exports.uploadFile = async (req, res) => {
       });
     }
 
-    // Upload to Cloudinary
-    const result = await cloudinary.uploader.upload(req.file.path, {
-      folder: 'ticket_hub',
-      resource_type: 'auto'
-    });
-
-    // Delete local temp file
-    try {
-      fs.unlinkSync(req.file.path);
-    } catch (err) {
-      console.warn("Could not delete local temp file:", err);
-    }
-
-    const fileUrl = result.secure_url;
+    // Local static file path served via /images in index.js
+    const fileUrl = `/images/${req.file.filename}`;
     const user = req.user;
 
     // Save metadata to Attachment table
@@ -55,14 +35,6 @@ exports.uploadFile = async (req, res) => {
     });
   } catch (error) {
     console.error("Error uploading file:", error);
-    // Cleanup temp file in case of error
-    if (req.file && req.file.path) {
-      try {
-        fs.unlinkSync(req.file.path);
-      } catch (err) {
-        // ignore
-      }
-    }
     res.status(500).json({
       status: "error",
       message: "Internal server error"
